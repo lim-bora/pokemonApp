32b26775b1be6260c4e065f91c71c6f85bcae336
임보라 <imbora@imbolaui-MacBookAir.local>
2024-08-21 17:37:00 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import './Reset.css'
+
+function App() {
+
+
+  return (
+    <div className="pokemonApp">
+      
+    </div>
+  )
+}
+
+export default App
